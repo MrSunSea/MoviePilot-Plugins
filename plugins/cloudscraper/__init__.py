d79595b26cdac7e80a0a337829a6ae6a1dce530a
SunSea <zhangyang7mail@163.com>
2024-03-28 21:05:03 +0800

    level =》 4

--- plugins/cloudscraper/__init__.py
+++ plugins/cloudscraper/__init__.py
@@ -68,7 +68,7 @@ class CloudScraper(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "cloudscraper_"
     # 加载顺序
-    plugin_order = 1
+    plugin_order = 4
     # 可使用的用户级别
     auth_level = 1
 
